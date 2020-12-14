e70ffcd407e23dfbfcc77ec79e35e9a23e23e7b4
Oliver Herrmann <herrmann@id-on.de>
2020-12-14 17:12:44 +0100

    Improve day 12
    - parse with Parsec
    - remove unnecessary state Monad

--- 12/1.hs
+++ 12/1.hs
@@ -1,40 +1,37 @@
-import Data.Functor ((<&>))
-import Data.Maybe (listToMaybe,fromMaybe,mapMaybe)
-import Control.Monad.State (evalState, MonadState(put, get), State)
+import Text.Parsec ((<|>))
+import qualified Text.Parsec as P
 
 data Dir = N | E | S | W deriving (Show, Read, Enum)
 data Command = Move Dir | L | R | F deriving (Show, Read)
 type Instr = (Command, Int)
 
-main = (readFile "input.txt" <&> lines) >>= (print . run)
+main = readFile "input.txt" >>= (print . run . lines)
 
-run :: [String] -> Int
-run xs = evalState move (0,0,E,xs') where xs' = map parse xs
+-- run :: [String] -> Int
+run xs = move 0 0 E xs'
+    where xs' = case parse xs of
+            Right xs' -> xs'
+            Left err -> error $ show err
 
-parse :: String -> Instr
-parse s = (readCommand [head s], read $ tail s) where
-    readCommand s = head $ mapMaybe ($ s) [fmap Move . readDir,readLRFCommand]
-    readDir = listToMaybe . map fst . reads :: String -> Maybe Dir
-    readLRFCommand = listToMaybe . map fst . reads :: String -> Maybe Command
+parse :: [String] -> Either P.ParseError [Instr]
+parse = P.parse parseInstructions ""  . unwords where
+    parseInstructions = parseInstruction `P.sepEndBy1` P.space
+    parseInstruction = (,) <$> parseCommand <*> parseNum
+    parseCommand = P.try (parseMove <|> parseLFRCommand)
+    parseMove = Move <$> (read <$> P.many1 (P.oneOf "NESW"))
+    parseLFRCommand = read <$> P.many1 (P.oneOf "LRF")
+    parseNum = read <$> P.many1 P.digit
 
-type S = (Int,Int,Dir,[Instr]) -- x,y,current direction, remaining instructions
-
-move :: State S Int
-move = do
-    (x,y,d,is) <- get
-    let (x',y',d') = move' x y d (fromMaybe (F,0) $ listToMaybe is)
-    put (x',y',d',tail is)
-    if null is then return (abs x' + abs y') else move
-
-move' :: Int -> Int -> Dir -> Instr -> (Int,Int,Dir)
-move' x y d (Move N,n) = (x,y-n,d)
-move' x y d (Move S,n) = (x,y+n,d)
-move' x y d (Move W,n) = (x-n,y,d)
-move' x y d (Move E,n) = (x+n,y,d)
-move' x y d (L,n)
-    | n > 0 = move' x y (toEnum ((fromEnum d + 3) `mod` 4)) (L,n-90)
-    | otherwise = (x,y,d)
-move' x y d (R,n)
-    | n > 0 = move' x y (toEnum ((fromEnum d + 1) `mod` 4)) (R,n-90)
-    | otherwise = (x,y,d)
-move' x y d (F,n) = move' x y d (Move d, n)
+move :: Int -> Int -> Dir -> [Instr] -> Int
+move x y _ [] = abs x + abs y
+move x y d ((Move N,n):xs) = move x (y-n) d xs
+move x y d ((Move S,n):xs) = move x (y+n) d xs
+move x y d ((Move W,n):xs) = move (x-n) y d xs
+move x y d ((Move E,n):xs) = move (x+n) y d xs
+move x y d ((L,n):xs)
+    | n > 0 = move x y (toEnum ((fromEnum d + 3) `mod` 4)) ((L,n-90):xs)
+    | otherwise = move x y d xs
+move x y d ((R,n):xs)
+    | n > 0 = move x y (toEnum ((fromEnum d + 1) `mod` 4)) ((R,n-90):xs)
+    | otherwise = move x y d xs
+move x y d ((F,n):xs) = move x y d ((Move d, n):xs)
